75e07936d28fa1acd258a9ee9052060f4a127943
Xonzoda Karimova <xonzodakarimova335@gmail.com>
2025-08-16 10:24:35 +0500

    update

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 // HTTP methods - GET, PUT, POST, DELETE
 
-// https://localhost:3000
+// https://localhost:8080
 
-const express = require("express")
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
-const app = express()
+app.use(express.json())
 
-const PORT = 8080
+app.get("/", (req, res) => {
+  res.status(200).json({message: "Hello "})
+});
 
-app.listen(PORT, () => console.log(`Listening on - https://localhost:${PORT}`))
+// - GET, PUT, POST, DELETE
+app.get("/post", (req, res) => {
+    const {firstname, lastname} = req.body
+    const message = `His full name -${req.body.firstname} ${req.body.lastname}`
+  res.send(message)
+});
+
+app.delete("/:id", (req,res) => {
+    const {id} = req.params
+    
+    res.send(id)
+})
+
+app.put("/:id", (req, res) => {
+    const {id} = req.params
+    const body = req.body
+
+    res.json({id, body})
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on - http://localhost:${PORT}`);
+});
+
+
+// domain - post - endpoint

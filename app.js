75e46f45d2f0e05c59499c651747ab7ec912c30f
Xonzoda Karimova <xonzodakarimova335@gmail.com>
2025-08-16 09:25:12 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// HTTP methods - GET, PUT, POST, DELETE
+
+// https://localhost:3000
+
+const express = require("express")
+
+const app = express()
+
+const PORT = 8080
+
+app.listen(PORT, () => console.log(`Listening on - https://localhost:${PORT}`))

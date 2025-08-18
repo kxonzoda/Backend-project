2e00558021d58871d81def9879177a729c3ecf31
Xonzoda Karimova <xonzodakarimova335@gmail.com>
2025-08-18 09:25:29 +0500

    Fix

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
 // HTTP methods - GET, PUT, POST, DELETE
 
-// https://localhost:8080
+// http://localhost:8080
 
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const mongoose = require("mongoose");
+const postModel = require("./models/post.model");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({message: "Hello "})
+app.get("/", async (req, res) => {
+  try {
+    const allPosts = await postModel.find()
+    res.status(200).json(allPosts)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 // - GET, PUT, POST, DELETE
-app.get("/post", (req, res) => {
-    const {firstname, lastname} = req.body
-    const message = `His full name -${req.body.firstname} ${req.body.lastname}`
-  res.send(message)
+app.post("/", async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const newPost = await postModel.create({ title, body });
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.delete("/:id", (req,res) => {
-    const {id} = req.params
-    
-    res.send(id)
-})
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+});
 
 app.put("/:id", (req, res) => {
-    const {id} = req.params
-    const body = req.body
-
-    res.json({id, body})
-})
-
-const DB_URL="mongodb+srv://TheFalseGenius:TheFalseGenius@expresscluster.rccvzxi.mongodb.net/?retryWrites=true&w=majority&appName=Expresscluster"
-
-app.listen(PORT, () => {
-  console.log(`Listening on - http://localhost:${PORT}`);
+  const { id } = req.params;
+  const body = req.body;
+  res.json({ id, body });
 });
 
+const PORT = process.env.PORT = 8080
+
+
+const bootstrap = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connect DB");
+    app.listen(PORT, () => {
+      console.log(`Listening on - http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error connecting with DB: ${error}`);
+  }
+};
+
+bootstrap();
 
 // domain - post - endpoint
